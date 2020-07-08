1dc172d00f8014428af335970369231acd3246da
darkgeek <mohamedshawky911@gmail.com>
2020-07-08 03:55:52 +0200

    complete connection between pose estimation and grasping components

--- components/viriatoGraspingPyrep/src/specificworker.py
+++ components/viriatoGraspingPyrep/src/specificworker.py
@@ -35,6 +35,7 @@ import numpy_indexed as npi
 from itertools import zip_longest
 import cv2
 import queue
+from scipy.spatial.transform import Rotation as R
 
 
 class SpecificWorker(GenericWorker):
@@ -63,11 +64,15 @@ class SpecificWorker(GenericWorker):
                                             "height": cam.get_resolution()[1],
                                             "depth": 3,
                                             "focal": cam.get_resolution()[0]/np.tan(np.radians(cam.get_perspective_angle())), 
+                                            "position": cam.get_position(), 
+                                            "rotation": cam.get_quaternion(), 
                                             "rgb": np.array(0), 
                                             "depth": np.ndarray(0)}
 
         self.grasping_objects = {}
-        self.grasping_objects["002_master_chef_can"] = Shape("can")
+        self.grasping_objects["002_master_chef_can"] = {"handler": Shape("can"),
+                                                        "sim_pose": None,
+                                                        "pred_pose": None}
 
 
     def compute(self):
@@ -75,15 +80,40 @@ class SpecificWorker(GenericWorker):
         while True:
             try:
                 self.pr.step()
+
+                # read arm camera RGB signal
                 cam = self.cameras["Gen3_depth_sensor"]
                 image_float = cam["handle"].capture_rgb()
                 depth = cam["handle"].capture_depth()
                 image = cv2.normalize(src=image_float, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
                 cam["rgb"] = RoboCompObjectPoseEstimation.TImage(width=cam["width"], height=cam["height"], depth=3, focalx=cam["focal"], focaly=cam["focal"], image=image.tobytes())
+                
+                # get objects's poses from simulator
+                for obj_name in self.grasping_objects.keys():
+                    self.grasping_objects[obj_name]["sim_pose"] = self.grasping_objects[obj_name]["handler"].get_pose()
+                
+                # get objects' poses from RGB
+                pred_poses = self.objectposeestimation_proxy.getObjectPose(cam["rgb"])
+                for pose in pred_poses:
+                    if pose.objectname in self.grasping_objects.keys():
+                        obj_trans = [pose.x, pose.y, pose.z]
+                        obj_rot = [pose.rx, pose.ry, pose.rz]
+                        obj_pose = self.process_pose(obj_trans, obj_rot)
+                        self.grasping_objects[pose.objectname]["pred_pose"] = obj_pose
+                
             except Ice.Exception as e:
                 print(e)
         return True
 
+
+    def process_pose(self, obj_trans, obj_rot):
+        # convert an object pose from camera frame to world frame
+        final_trans = obj_trans + self.cameras["Gen3_depth_sensor"]["position"]
+        cam_rot_mat = R.from_quat(self.cameras["Gen3_depth_sensor"]["rotation"]).as_matrix()
+        obj_rot_mat = R.from_euler('xyz', obj_rot).as_matrix()
+        final_rot_mat = np.matmul(obj_rot_mat, cam_rot_mat)
+        final_rot = R.from_matrix(final_rot_mat).as_quat()
+        return list(final_trans).extend(list(final_rot))
 
     ######################
     # From the RoboCompObjectPoseEstimation you can call this methods:
