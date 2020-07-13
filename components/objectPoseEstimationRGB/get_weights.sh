322ea4304bf07bd4efe378b89805ddc454a25d40
darkgeek <mohamedshawky911@gmail.com>
2020-07-13 06:17:07 +0200

    finalize RGB pose estimation component

--- components/objectPoseEstimationRGB/get_weights.sh
+++ components/objectPoseEstimationRGB/get_weights.sh
@@ -2,4 +2,4 @@
 # download segmentation-based pose DNN weights
 
 mkdir src/models
-wget "https://drive.google.com/file/d/1MUG7aqhGXlubtSJHlmY0xbFdLRrAOTX-/view?usp=sharing" -O src/models/ckpt_final.pth
+wget "https://drive.google.com/file/d/1FheGfo7tiQPEpOq9xp3EE8iPAJxWloo2/view?usp=sharing" -O src/models/ckpt_final.pth
