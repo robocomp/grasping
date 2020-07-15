188439f6c10204cb351c7ef6dd4be84e09321e51
darkgeek <mohamedshawky911@gmail.com>
2020-07-15 07:24:56 +0200

    some folder rename

--- components/objectPoseEstimationRGB/get_weights.sh
+++ components/objectPoseEstimationRGB/get_weights.sh
@@ -2,4 +2,4 @@
 # download segmentation-based pose DNN weights
 
 mkdir src/models
-wget "https://drive.google.com/file/d/1FheGfo7tiQPEpOq9xp3EE8iPAJxWloo2/view?usp=sharing" -O src/models/ckpt_final.pth
+wget "https://drive.google.com/file/d/1N-qI5dqFVSNryZ0WwKlLn7npDkyVs_eh/view?usp=sharing" -O src/models/ckpt_final.pth
